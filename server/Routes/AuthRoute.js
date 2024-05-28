4cdf8948dd4b97f50a094a3c8143af4cd01e4aeb
Ishita Sancheti <b22ee093@iitj.ac.in>
2024-05-28 22:34:38 +0530

    f

--- server/Routes/AuthRoute.js
+++ server/Routes/AuthRoute.js
@@ -1,7 +1,9 @@
-const { Signup, Login } = require('../Controllers/AuthController')
-const router = require('express').Router()
+const { Signup, Login } = require('../Controllers/AuthController');
+const { userVerification } = require('../Middlewares/AuthMiddleware'); // Adjust the path as necessary
+const router = require('express').Router();
 
-router.post('/signup', Signup)
-router.post('/login', Login)
-router.post('/',userVerification)
-module.exports = router
+router.post('/signup', Signup);
+router.post('/login', Login);
+router.post('/', userVerification);
+
+module.exports = router;
